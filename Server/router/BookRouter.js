b36d5e7167624edb7d14917cab28ea94188b98ba
root <root@DESKTOP-NK4VINM>
2023-09-17 14:40:52 +0200

    add files in folder server

--- Server/router/BookRouter.js
+++ Server/router/BookRouter.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const router = express.Router();
-const Book = require('../modals/BookModal');
-
+const express = require('express')
+const router = express.Router()
+const Book = require('../modals/BookModal')
 
 router.get('/getallBooks', async (req, res) => {
   try {
@@ -17,17 +16,17 @@ router.get('/getallBooks', async (req, res) => {
 
 router.post('/getBookById', async (req, res) => {
   try {
-    console.log("heollo world",req.body)
+    console.log('heollo world', req.body)
     const BookInfo = await Book.find({ _id: req.body.bookId })
-//    const BookInfo = await Book.find({ _id: '64f8b4516811b034ba590a95' })
+    //    const BookInfo = await Book.find({ _id: '64f8b4516811b034ba590a95' })
     console.log(BookInfo)
     res.status(200).send(BookInfo[0])
   } catch (err) {
-    console.log(err);
+    console.log(err)
     res.status(404).json({
       status: 'failed',
       message: err,
     })
   }
 })
-module.exports = router;
+module.exports = router
